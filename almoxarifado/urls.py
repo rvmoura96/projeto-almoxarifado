77f105ce21a90c0054a06a534a28ace831e6784e
Renan Moura <rvmoura.96@gmail.com>
2017-10-09 21:41:48 -0300

    Refactor templates

--- almoxarifado/urls.py
+++ almoxarifado/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.general_list, name='general_list'),
+    url(r'^equips/$', views.equip_list, name='equip_list'),
     url(r'^equip/(?P<pk>\d+)$', views.equip_detail, name='equip_detail'),
     url(r'^equip/new/$', views.item_new, name='item_new'),
     url(r'^equip/(?P<pk>\d+)/edit/$', views.item_edit, name='item_edit'),
