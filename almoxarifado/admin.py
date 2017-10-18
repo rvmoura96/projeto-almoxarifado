f443fae1845cd281f5382d38b8573d39b0abca5a
Renan Moura <rvmoura.96@gmail.com>
2017-10-18 21:20:49 -0200

    Refactor models, criação model generica para modelo, fabricante, tipo de equipamento e tipo de item

--- almoxarifado/admin.py
+++ almoxarifado/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Equipamento, Item, Notebook
+from .models import Equipamento, Item, Tipo, Tipo_Itens, Modelo, Fabricante
 # Register your models here.
 
+admin.site.register(Tipo)
+admin.site.register(Tipo_Itens)
+admin.site.register(Modelo)
+admin.site.register(Fabricante)
 admin.site.register(Equipamento)
-admin.site.register(Notebook)
 admin.site.register(Item)
--- almoxarifado/models.py
+++ almoxarifado/models.py
@@ -2,32 +2,44 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
+class Tipo(models.Model):
+    tipo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.tipo
+
+class Tipo_Itens(models.Model):
+    tipo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.tipo
+
+class Modelo(models.Model):
+    modelo = models.CharField(max_length=170)
+
+    def __str__(self):
+        return self.modelo
+
+class Fabricante(models.Model):
+    fabricante = models.CharField(max_length=90)
+
+    def __str__(self):
+        return self.fabricante
+
 class Equipamento(models.Model):
+    
     disponivel = 'Disponivel'
     indisponivel = 'Indisponivel'
-    notebook = 'Notebook'
-    desktop = 'Desktop'
-    projetor = 'Projetor'
-    impressora = 'Impressora'
-    monitor = 'Monitor'
-
-    tipo_choices = (
-        (notebook, 'Notebook'),
-        (desktop, 'Desktop'),
-        (projetor, 'Projetor'),
-        (impressora, 'Impressora'),
-        (monitor, 'Monitor'),
-
-    )
 
     status_choices = (
         (disponivel, 'Disponível'),
         (indisponivel, 'Indisponível'),
     )
 
-    tipo = models.CharField(max_length=50, choices=tipo_choices, default=notebook)
-    modelo = models.CharField(max_length=170)
-    fabricante = models.CharField(max_length=90, default= None)
+    tipo = models.ForeignKey(Tipo)
+    modelo = models.ForeignKey(Modelo)
+    fabricante = models.ForeignKey(Fabricante)
     ativo_imobilizado = models.PositiveIntegerField(default=0, unique=True)
     serial_number = models.CharField(max_length=30, unique=True, default= None)
     status = models.CharField(max_length=12, choices=status_choices, default=disponivel)
@@ -38,34 +50,18 @@ class Equipamento(models.Model):
     def __str__(self):
         return self.modelo
 
-class Notebook(Equipamento):
-    memoria = models.PositiveIntegerField(default=0)
-    processador = models.CharField(max_length=170)
-    hd = models.PositiveIntegerField(default=0)
-
-    def __str__(self):
-        return self.modelo
-
 class Item(models.Model):
     disponivel = 'Disponivel'
     indisponivel = 'Indisponivel'
-
-    perifericos = 'Periféricos'
-    consumiveis = 'Consumíveis'
 
     status_choices = (
         (disponivel,' Disponível'),
         (indisponivel, 'Indisponível'),
     )
 
-    tipo_choices = {
-        (consumiveis, 'Consumíveis'),
-        (perifericos, 'Periféricos'),
-    }
-
-    tipo = models.CharField(max_length=12, choices=tipo_choices, default=consumiveis)
+    tipo = models.ForeignKey(Tipo_Itens)
     item = models.CharField(max_length=170)
-    fabricante = models.CharField(max_length=90, default= None)
+    fabricante = models.ForeignKey(Fabricante)
     quantidade = models.PositiveIntegerField(default=0)
     status = models.CharField(max_length=12, choices=status_choices, default=disponivel)
     prateleira = models.PositiveIntegerField(default=0)
